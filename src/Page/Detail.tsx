4d457d85de59de169018d79f5ea286d65bfa0a40
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2022-11-18 10:03:18 +0900

    [Add] 통신으로 CRUD 기능 결합 확인

--- src/Page/Detail.tsx
+++ src/Page/Detail.tsx
@@ -1,11 +1,45 @@
 import axios from "axios"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { useParams } from "react-router-dom"
 
 
 function Detail() {
     const params = useParams()
     const [data, setData] = useState<any>({})
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState('')
+    const [subDescription, setSubDescription] = useState('')
+
+    const onChangePrice = useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
+        setPrice(e.target.value)
+    },[])
+
+    const onChangeDescription = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setDescription(e.target.value)
+    },[])
+
+    const onChangeSubDescription = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setSubDescription(e.target.value)
+    },[])
+
+    const onPostModifySubmit = useCallback((e:React.FormEvent) => {
+        e.preventDefault()
+        axios.patch(`http://localhost:8001/post/${params.title}`, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            price,
+            description,
+            subDescription
+        })
+        .then((res) => {
+            alert('111')
+        })
+        .then((error) => {
+            console.error(error)
+        })
+    },[price, description, subDescription])
+    
 
     useEffect(() => {
         async function fetchData() {
@@ -15,18 +49,25 @@ function Detail() {
         fetchData()
     } , [])
 
+    const onDeleteButton = () => {
+        async function DeleteData() {
+            await axios.delete(`http://localhost:8001/post/${params.title}`)
+        }
+        DeleteData()
+    }
+
         return (
         <div>
-          
             <p>{data.id}</p>
             <p>{data.title}</p>
-            {/* {data.length >= 0 && data.map((Td) => (
-                <div key={Td.id} style={{border:'1px solid #dbdbdb'}}>   
-                 <p>{Td.id}</p> 
-                 <p>{Td.title}</p> 
-                 <p>{Td.description}</p>
-                </div>
-            ))}  */}
+            <button onClick={onDeleteButton}>삭제</button>
+
+            <div>
+    <input name='description' type='text' onChange={onChangeDescription} tabIndex={2}/>
+    <input name='subDescription' type='text' onChange={onChangeSubDescription} tabIndex={3} />
+    <input name='price' type='text' onChange={onChangePrice} tabIndex={4}/>
+    <button type="submit" onClick={onPostModifySubmit}>게시</button>
+            </div>
         </div>
     )
 }
